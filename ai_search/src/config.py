6edd0a8cd997d4d384d880a771f9c2e817888909
Johannes Medagbe <johanmedagbe@gmail.com>
2026-02-04 16:55:38 +0300

    Draft RAG system for AI search

---
+++ ai_search/src/config.py
@@ -0,0 +1,80 @@
+"""
+This module sets up configurations for the RAG
+It also provides different constructs to interact with the LLM
+
+Embedding model = Qwen/Qwen3-Embedding-0.6B
+"""
+
+
+import dspy
+import torch
+
+
+EMBED_MODEL = "Qwen/Qwen3-Embedding-0.6B"
+
+
+CORPUS_PATH = os.getenv("CORPUS_PATH")
+if CORPUS_PATH is None:
+    raise ValueError("CORPUS_PATH must be specified to find corpus")
+PCORPUS_PATH = os.getenv("PCORPUS_PATH")
+if PCORPUS_PATH is None:
+    raise ValueError("PCORPUS_PATH must be specified to read corpus")
+DB_PATH = os.getenv("DB_PATH")
+if DB_PATH is None:
+    raise ValueError("DB_PATH must be specified to access database")
+QUERY = os.getenv("QUERY")
+if QUERY is None:
+    raise ValueError("QUERY must be specified for program to run")
+SEED = os.getenv("SEED")
+if SEED is None:
+    raise ValueError("SEED must be specified for reproducibility")
+MODEL_NAME = os.getenv("MODEL_NAME")
+if MODEL_NAME is None:
+    raise ValueError("MODEL_NAME must be specified - either proprietary or local"
+    )
+MODEL_TYPE = os.getenv("MODEL_TYPE")
+if MODEL_TYPE is None:
+    raise ValueError("MODEL_TYPE must be specified")
+
+
+torch.manual_seed(SEED)
+
+if torch.cuda.is_available():
+    torch.cuda.manual_seed_all(SEED)
+
+    
+if int(MODEL_TYPE) == 0:
+    llm = dspy.LM(
+        model=f"openai/{MODEL_NAME}",
+        api_base="http://localhost:7501/v1",
+        api_key="local",
+        model_type="chat",
+        max_tokens=1_000,
+        n_ctx=5_000,
+        seed=2_025,
+        temperature=0,
+        verbose=False,
+    )
+elif int(MODEL_TYPE) == 1:
+    API_KEY = os.getenv("API_KEY")
+    if API_KEY is None:
+        raise ValueError("Valid API_KEY must be specified to use the proprietary model")
+    llm = dspy.LM(
+        MODEL_NAME,
+        api_key=API_KEY,
+        max_tokens=1_000,
+        temperature=0,
+        verbose=False,
+    )
+else:
+    raise ValueError("MODEL_TYPE must be 0 or 1")
+
+
+dspy.configure(lm=llm)
+
+class Generate(dspy.Signature):
+    context: list = dspy.InputField()
+    input_text: str = dspy.InputField()
+    answer: str = dspy.OutputField()
+
+generate = dspy.Predict(Generate)
