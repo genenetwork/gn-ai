33ccf559e7c574de2eddb577a7035e68c37f9340
Johannes Medagbe <johanmedagbe@gmail.com>
2026-02-25 16:49:31 +0300

    Fix in tool calling agent

--- aisearch/src/gnais/agent/agent.py
+++ aisearch/src/gnais/agent/agent.py
@@ -1,14 +1,11 @@
 """Agent with sparql tool calling for AI search in GeneNetwork"""
 
-__all__ = "AISearch"
-
-
 import os
 from typing import Any
 
 import dspy
 from gnais.rag.config import ListInformation
-from SPARQLWrapper import SPARQLWrapper
+from SPARQLWrapper import SPARQLWrapper, JSON
 
 TTL_PATH = os.getenv("TTL_PATH")
 if TTL_PATH is None:
@@ -18,41 +15,41 @@ if TTL_PATH is None:
 
 
 def extract_schema(ttl_path: str) -> tuple[list, list]:
-    ttl_files = [os.path.join(dir_path, ttl) for ttl in os.listdir(ttl_path)]
+    ttl_files = [os.path.join(ttl_path, ttl) for ttl in os.listdir(ttl_path) if 'ttl' in ttl]
     prefixes = []
-    properties = []
+    predicates = []
     for ttl in ttl_files:
         with open(ttl) as f:
             contents = f.readlines()
-        for content in contents:
-            if content.startswith("@") and content not in prefixes:
-                prefixes.append(content)
-            else:
-                prop = content.split()[1]
-                if prop not in properties:
-                    properties.append(prop)
-    return prefixes, properties
+            for content in contents:
+                content = content.strip()
+                if content.startswith("@") and content not in prefixes:
+                    prefixes.append(content)
+                elif len(content) != 0:
+                    predicate = content.split()[1]
+                    if predicate not in predicates:
+                        predicates.append(predicate)
+    return prefixes, predicates
 
 
-class Translation(dspy.signature):
-    user_query: str = dspy.InputField()
-    prefixes: list[str] = dspy.InputField("RDF prefixes for namespaces")
-    properties: list[str] = dspy.InputField(
-        "Properties linking subject and object in RDF triples"
-    )
+class QueryTranslation(dspy.Signature):
+    original_query: str = dspy.InputField()
+    rdf_prefixes: list[str] = dspy.InputField()
+    triple_predicates: list[str] = dspy.InputField()
     translated_query: str = dspy.OutputField(
-        desc="SPARQL query corresponding to the user query for fetching requested data given RDF properties and prefixes"
+        desc="SPARQL query corresponding to user query for fetching requested data given RDF schema inferred from RDF prefixes and triple predicates"
     )
 
 
-translate = dspy.Predict(Translation)
+translate_query = dspy.Predict(QueryTranslation)
 
 
-def fetch_data(user_query: str) -> Any:
-    sparql = SPARQLWrapper("https://rdf.genenetwork.org/sparql")
-    prefixes, properties = extract_schema(TTL_PATH)
-    sparql_query = translate(
-        user_query=user_query, prefixes=prefixes, properties=properties
+def fetch_data(query: str) -> Any:
+    sparql = SPARQLWrapper("https://rdf.genenetwork.org/sparql/")
+    sparql.setReturnFormat(JSON)
+    prefixes, predicates = extract_schema(TTL_PATH)
+    sparql_query = translate_query(
+        original_query=query, rdf_prefixes=prefixes, triple_predicates=predicates
     ).get("translated_query")
     sparql.setQuery(sparql_query)
     return sparql.queryAndConvert()
@@ -84,8 +81,9 @@ class AISearch(dspy.Module):
         self.react = dspy.ReAct(
             signature=ReactSig,
             tools=self.tools,
-            max_iters=50,  # maximum number of steps for reasoning and tool calling
+            max_iters=20,  # maximum number of steps for reasoning and tool calling
         )
 
     def forward(self, query: str):
         return self.react(query=query)
+
