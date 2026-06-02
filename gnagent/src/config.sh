2da6391245d42a40af3cdb682a9eb56a42272879
Johannes Medagbe <johanmedagbe@gmail.com>
2026-06-02 09:38:55 +0300

    Clean: remove old gnagent package

--- gnagent/src/config.sh
+++
@@ -1,4 +0,0 @@
-# Set sglang server for local LLM
-# No need to run this file if using proprietary model
-
-CUDA_VISIBLE_DEVICES=0 python -m sglang.launch_server --port 7501 --model-path $MODEL_NAME --disable-cuda-graph --attention-backend flashinfer
