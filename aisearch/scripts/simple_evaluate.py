4a9b43ae3e827630754f64dfbff47b1e0359f4fb
Johannes Medagbe <johanmedagbe@gmail.com>
2026-07-06 23:51:40 +0300

    New: track token count in simple evaluation script

--- aisearch/scripts/simple_evaluate.py
+++ aisearch/scripts/simple_evaluate.py
@@ -13,13 +13,22 @@ import pandas as pd
 import torch
 from dotenv import load_dotenv
 from gnais.config import Config
-from gnais.evaluation.utils import (agent_digest, get_dataset,
-                                    graph_rag_digest, hybrid_digest, mark,
-                                    rag_digest)
+from gnais.evaluation.utils import (
+    agent_digest,
+    get_dataset,
+    graph_rag_digest,
+    hybrid_digest,
+    mark,
+    rag_digest,
+)
 from gnais.search.agent import agent_search
 from gnais.search.classification import classify_search
-from gnais.search.corpus import (create_ensemble_retriever, get_chroma_db,
-                                 get_docs, init_chroma_db)
+from gnais.search.corpus import (
+    create_ensemble_retriever,
+    get_chroma_db,
+    get_docs,
+    init_chroma_db,
+)
 from gnais.search.grag import graph_rag_search
 from gnais.search.rag import rag_search
 from gnais.search.ragent import hybrid_search
@@ -58,27 +67,31 @@ def make_program(
 def run_eval(
     runner: Any, evaluation_set: list[dspy.Example], judge_llm: dspy.LM
 ) -> dict[str, float]:
-    precisions, recalls, f1s, speeds = [], [], [], []
+    precisions, recalls, f1s, speeds, n_tokens = [], [], [], [], []
     for example in evaluation_set:
         query = example.get("query")
         true_response = example.get("answer")
-        
+
         start = time.time()
-        generated_response = runner(query=query).get("answer")
+        result = runner(query=query)
         end = time.time()
-        
+        generated_response = result.get("answer")
+        n_token = list(result.get_lm_usage().values())[0]["total_tokens"]
+
         with dspy.context(lm=judge_llm):
             precision, recall, f1 = mark(query, generated_response, true_response)
         precisions.append(precision)
         recalls.append(recall)
         f1s.append(f1)
-        speeds.append(end-start)
+        speeds.append(end - start)
+        n_tokens.append(n_token)
 
     metrics = {
         "precision": np.mean(precisions).item(),
         "recall": np.mean(recalls).item(),
         "f1": np.mean(f1s).item(),
         "speed": np.mean(speeds).item(),
+        "n_token": np.mean(n_tokens).item(),
     }
     return metrics
 
@@ -113,7 +126,7 @@ if __name__ == "__main__":
         cache=False,
         verbose=False,
     )
-    dspy.configure(lm=llm)
+    dspy.configure(lm=llm, track_usage=True)
 
     judge_llm = dspy.LM(
         model=JUDGE_MODEL,
