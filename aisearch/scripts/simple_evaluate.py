5b15697e4f4e897b03a688660d9c14042bf444d9
Johannes Medagbe <johanmedagbe@gmail.com>
2026-07-06 23:04:51 +0300

    New: login time with performance metrics

--- aisearch/scripts/simple_evaluate.py
+++ aisearch/scripts/simple_evaluate.py
@@ -3,6 +3,7 @@
 import argparse
 import asyncio
 import os
+import time
 import uuid
 from typing import Any
 
@@ -12,22 +13,13 @@ import pandas as pd
 import torch
 from dotenv import load_dotenv
 from gnais.config import Config
-from gnais.evaluation.utils import (
-    agent_digest,
-    get_dataset,
-    graph_rag_digest,
-    hybrid_digest,
-    mark,
-    rag_digest,
-)
+from gnais.evaluation.utils import (agent_digest, get_dataset,
+                                    graph_rag_digest, hybrid_digest, mark,
+                                    rag_digest)
 from gnais.search.agent import agent_search
 from gnais.search.classification import classify_search
-from gnais.search.corpus import (
-    create_ensemble_retriever,
-    get_chroma_db,
-    get_docs,
-    init_chroma_db,
-)
+from gnais.search.corpus import (create_ensemble_retriever, get_chroma_db,
+                                 get_docs, init_chroma_db)
 from gnais.search.grag import graph_rag_search
 from gnais.search.rag import rag_search
 from gnais.search.ragent import hybrid_search
@@ -66,20 +58,27 @@ def make_program(
 def run_eval(
     runner: Any, evaluation_set: list[dspy.Example], judge_llm: dspy.LM
 ) -> dict[str, float]:
-    precisions, recalls, f1s = [], [], []
+    precisions, recalls, f1s, speeds = [], [], [], []
     for example in evaluation_set:
         query = example.get("query")
         true_response = example.get("answer")
+        
+        start = time.time()
         generated_response = runner(query=query).get("answer")
+        end = time.time()
+        
         with dspy.context(lm=judge_llm):
             precision, recall, f1 = mark(query, generated_response, true_response)
         precisions.append(precision)
         recalls.append(recall)
         f1s.append(f1)
+        speeds.append(end-start)
+
     metrics = {
         "precision": np.mean(precisions).item(),
         "recall": np.mean(recalls).item(),
         "f1": np.mean(f1s).item(),
+        "speed": np.mean(speeds).item(),
     }
     return metrics
 
