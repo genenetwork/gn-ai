abbb949d8f3593825ea59ecb6f11bea9d6b21f8d
Johannes Medagbe <johanmedagbe@gmail.com>
2026-09-12 11:58:23 +0300

    Fix: use all data points in boxplot

--- aisearch/scripts/plot_function.py
+++ aisearch/scripts/plot_function.py
@@ -23,21 +23,31 @@ if OUTPUT2_PATH is None:
     raise FileNotFoundError("Set OUTPUT2_PATH or path to save final heatmap for models")
 
 
-def format_results(path: str) -> pd.DataFrame:
+def format_results(path: str) -> tuple[pd.DataFrame]:
     files = os.listdir(path)
-    df_list = []
+    full_list = []
+    aggr_list = []
     for f in files:
         model = f.strip("_dspy_results.csv")
         data = pd.read_csv(f"{path}/{f}")
-        new = pd.DataFrame(
+
+        aggr = pd.DataFrame(
             data.aggregate("median"), columns=["satisfaction frequency (%)"]
         )
-        new["model"] = model
-        new["system"] = list(new.index)
-        df_list.append(new)
-    df_concat = pd.concat(df_list)
-    df_concat["model_order"] = pd.Categorical(
-        df_concat["model"],
+        aggr["model"] = model
+        aggr["system"] = list(aggr.index)
+        aggr_list.append(aggr)
+
+        full = data.transpose()
+        full = pd.concat([full[column] for column in full.columns])
+        full_df = pd.DataFrame(full, columns=["satisfaction frequency (%)"])
+        full_df["model"] = model
+        full_df["system"] = list(full.index)
+        full_list.append(full_df)
+
+    full_concat = pd.concat(full_list)
+    full_concat["model_order"] = pd.Categorical(
+        full_concat["model"],
         categories=[
             "gptoss_20b",
             "qwen3_30b",
@@ -48,18 +58,46 @@ def format_results(path: str) -> pd.DataFrame:
         ],
         ordered=False,
     )
-    df_concat["system_order"] = pd.Categorical(
-        df_concat["system"],
+    full_concat["system_order"] = pd.Categorical(
+        full_concat["system"],
         categories=["base", "rag", "graph rag", "agent", "hybrid"],
         ordered=False,
     )
-    return df_concat.sort_values(by=["system_order", "model_order"])
+
+    aggr_concat = pd.concat(aggr_list)
+    aggr_concat["model_order"] = pd.Categorical(
+        aggr_concat["model"],
+        categories=[
+            "gptoss_20b",
+            "qwen3_30b",
+            "haiku_4.5",
+            "qwen3_30b_opus_4.8",
+            "haiku_4.5_opus_4.8",
+            "opus_4.8",
+        ],
+        ordered=False,
+    )
+    aggr_concat["system_order"] = pd.Categorical(
+        aggr_concat["system"],
+        categories=["base", "rag", "graph rag", "agent", "hybrid"],
+        ordered=False,
+    )
+    return full_concat.sort_values(
+        by=["system_order", "model_order"]
+    ), aggr_concat.sort_values(by=["system_order", "model_order"])
 
 
-def plot_results(data: pd.DataFrame, output1_path: str, output2_path: str):
-    sns.boxplot(data=data, x="system", y="satisfaction frequency (%)", color="#708090")
+def plot_results(
+    full_data: pd.DataFrame,
+    aggr_data: pd.DataFrame,
+    output1_path: str,
+    output2_path: str,
+):
+    sns.boxplot(
+        data=full_data, x="system", y="satisfaction frequency (%)", color="#708090"
+    )
     sns.swarmplot(
-        data=data,
+        data=full_data,
         x="system",
         y="satisfaction frequency (%)",
         hue="model",
@@ -67,7 +105,7 @@ def plot_results(data: pd.DataFrame, output1_path: str, output2_path: str):
     )
     plt.savefig(output1_path, dpi=1000)
     plt.show()
-    heatmap_data = data.pivot(
+    heatmap_data = aggr_data.pivot(
         index="model", columns="system", values="satisfaction frequency (%)"
     )
     heatmap_data = heatmap_data.reindex(
@@ -92,5 +130,5 @@ def plot_results(data: pd.DataFrame, output1_path: str, output2_path: str):
 
 
 if __name__ == "__main__":
-    df = format_results(INPUT_PATH)
-    plot_results(df, OUTPUT1_PATH, OUTPUT2_PATH)
+    full_df, aggr_df = format_results(INPUT_PATH)
+    plot_results(full_df, aggr_df, OUTPUT1_PATH, OUTPUT2_PATH)
